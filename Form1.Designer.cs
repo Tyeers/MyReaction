15d447d0ce20f6ae1677dd285de3b17103f59d56
Tyeers <aqilapasya7@gmail.com>
2024-05-07 15:10:04 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace MyReaction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PlayBtn = new System.Windows.Forms.Button();
+            this.LeaderboardBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PlayBtn
+            // 
+            this.PlayBtn.Font = new System.Drawing.Font("Arial Rounded MT Bold", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayBtn.Location = new System.Drawing.Point(431, 409);
+            this.PlayBtn.Name = "PlayBtn";
+            this.PlayBtn.Size = new System.Drawing.Size(400, 80);
+            this.PlayBtn.TabIndex = 0;
+            this.PlayBtn.Text = "Play";
+            this.PlayBtn.UseVisualStyleBackColor = true;
+            this.PlayBtn.Click += new System.EventHandler(this.PlayBtn_Click);
+            // 
+            // LeaderboardBtn
+            // 
+            this.LeaderboardBtn.Font = new System.Drawing.Font("Arial Rounded MT Bold", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LeaderboardBtn.Location = new System.Drawing.Point(431, 504);
+            this.LeaderboardBtn.Name = "LeaderboardBtn";
+            this.LeaderboardBtn.Size = new System.Drawing.Size(400, 80);
+            this.LeaderboardBtn.TabIndex = 1;
+            this.LeaderboardBtn.Text = "Leaderboard";
+            this.LeaderboardBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.LeaderboardBtn);
+            this.Controls.Add(this.PlayBtn);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyReaction";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PlayBtn;
+        private System.Windows.Forms.Button LeaderboardBtn;
+    }
+}
+
